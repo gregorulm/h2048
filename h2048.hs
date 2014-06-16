8786785749c60fa50087aa5ccbad40a3e09e95fc
Gregor Ulm <gregorulm@users.noreply.github.com>
2014-06-16 16:52:30 +0200

    Update h2048.hs
    disabled input buffering; refactoring

--- h2048.hs
+++ h2048.hs
@@ -1,18 +1,24 @@
 {-
 
+h2048
 A Haskell implementation of 2048.
 
 Gregor Ulm
-2014-06-15
+
+last update:
+2014-06-16
 
 Please consult the file README for further information
 on this program.
-   
+
 -}
 
 import Prelude hiding (Left, Right)
+import Data.Char (toLower)
 import Data.List
+import System.IO
 import System.Random
+import Text.Printf
 
 data Move = Up | Down | Left | Right
 type Grid = [[Int]]   
@@ -28,9 +34,9 @@ merge xs = merged ++ padding
     where padding          = replicate (length xs - length merged) 0
           merged           = combine $ filter (/= 0) xs
           combine (x:y:xs) | x == y    = x * 2 : combine xs
-                           | otherwise = x     : combine (y:xs) 
+                           | otherwise = x     : combine (y:xs)
           combine x        = x
-                    
+
 move :: Grid -> Move -> Grid
 move grid Left  = map merge grid
 move grid Right = map (reverse . merge . reverse) grid
@@ -49,59 +55,68 @@ setSquare grid (row, col) val = pre ++ [mid] ++ post
           post = drop (row + 1) grid
 
 isMoveLeft :: Grid -> Bool
-isMoveLeft grid = sum possibilities > 0 
-    where possibilities = map (length . getZeroes . move grid) [Left, Right, Up, Down] 
+isMoveLeft grid = sum allChoices > 0 
+    where allChoices = map (length . getZeroes . move grid) directions
+          directions = [Left, Right, Up, Down]
 
 printGrid :: Grid -> IO ()
-printGrid grid = do putStr $ "\ESC[2J" ++ "\ESC[2J" --- clears the screen
-                    mapM_ (putStrLn . printRow) grid
+printGrid grid = do
+    putStr "\ESC[2J\ESC[2J\n" -- clears the screen
+    mapM_ (putStrLn . showRow) grid
 
-printRow :: [Int] -> String
-printRow (x:xs) = front ++ show x ++ printRow xs
-    where front = concat $ replicate (5 - length (show x)) " "  
-printRow []     = ""
+showRow :: [Int] -> String
+showRow = concatMap (printf "%5d")
 
-captureMove :: IO Move    
-captureMove = do 
-    inp <- getLine
-    case inp of
-        "w" -> return Up
-        "a" -> return Left
-        "s" -> return Down
-        "d" -> return Right
-        _   -> do putStrLn "Use WASD (lowercase) as input"
-                  captureMove  
+moves :: [(Char, Move)]
+moves = keys "wasd" ++ keys "chtn"
+    where keys chars = zip chars [Up, Left, Down, Right]
+
+captureMove :: IO Move
+captureMove = do
+    inp <- getChar
+    case lookup (toLower inp) moves of
+        Just x  -> return x
+        Nothing -> do putStrLn "Use WASD or CHTN as input"
+                      captureMove
 
 check2048 :: Grid -> Bool
 check2048 grid = [] /= filter (== 2048) (concat grid)
                 
 addTile :: Grid -> IO Grid
-addTile grid = do g <- newStdGen
-                  let candidates      = getZeroes grid
-                      pos             = head (randoms g :: [Int]) `mod` length candidates
-                      pick            = candidates!!pos
-                      val             = [2,2,2,2,2,2,2,2,2,4] !! (head (randoms g :: [Int]) `mod` 10)
-                      new_grid        = setSquare grid pick val
-                  return new_grid
+addTile grid = do 
+    let candidates = getZeroes grid
+    pick <- choose candidates
+    val  <- choose [2,2,2,2,2,2,2,2,2,4]
+    let new_grid = setSquare grid pick val
+    return new_grid
+
+choose :: [a] -> IO a
+choose xs = do
+    i <- randomRIO (0, length xs-1)
+    return (xs !! i)
 
 newGrid :: Grid -> IO Grid
-newGrid grid = do m <- captureMove    
-                  let new_grid = move grid m                         
-                  return new_grid
-                   
+newGrid grid = do
+    m <- captureMove    
+    let new_grid = move grid m                         
+    return new_grid
+
 gameLoop :: Grid -> IO ()
-gameLoop grid =
-    case isMoveLeft grid of
-            True  -> do printGrid grid
-                        if check2048 grid
-                        then putStrLn "You won!"
-                        else do new_grid <- newGrid grid                        
-                                if grid /= new_grid
-                                then do new <- addTile new_grid
-                                        gameLoop new
-                                else gameLoop grid
-            False -> do printGrid grid
-                        putStrLn "Game over"
+gameLoop grid
+    | isMoveLeft grid = do
+        printGrid grid
+        if check2048 grid
+        then putStrLn "You won!"
+        else do new_grid <- newGrid grid                        
+                if grid /= new_grid
+                then do new <- addTile new_grid
+                        gameLoop new
+                else gameLoop grid
+    | otherwise = do
+        printGrid grid
+        putStrLn "Game over"
 
 main :: IO ()
-main = gameLoop start
+main = do
+    hSetBuffering stdin NoBuffering
+    gameLoop start
